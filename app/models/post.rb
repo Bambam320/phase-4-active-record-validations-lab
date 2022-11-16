00b8b643de810caaa67aae8ecaf9ad0eca699e3c
Bambam320 <igor.mestechkin@gmail.com>
2022-11-15 22:13:56 -0500

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    unless title.to_s.match(/Won't Believe|Guess|Secret|Top [0-9]\+/)
+      errors.add(:title, "This is not sufficiently click-bait-y")        
+    end
+  end
+
 end
